4b3eb252a0e8e4aec7c73d8e17b6751820f9952e
Jeremy Fernández <jeremy.fernandez.castro@est.una.ac.cr>
2025-05-23 19:44:28 -0600

    Carpetas Services y Models

--- MediTrack.Frontend/Models/Preuba.cs
+++ MediTrack.Frontend/Models/Preuba.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MediTrack.Frontend.Models
 {
-    class Prueba
+    class Preuba
     {
     }
 }
--- MediTrack.Frontend/Services/Preuba.cs
+++ MediTrack.Frontend/Services/Preuba.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MediTrack.Frontend.Services
 {
-    class Prueba
+    class Preuba
     {
     }
 }
