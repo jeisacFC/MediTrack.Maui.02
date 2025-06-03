0733154ed16f58b209a346f934890358d2af59b3
Jeremy Fernández <jeremy.fernandez.castro@est.una.ac.cr>
2025-06-03 02:42:18 -0600

    Comenta el intento de usar la PantallaScan en la PantallaInicio (Voy a jalar los cambios de Sian a mi rama)

--- MediTrack.Frontend/Vistas/PantallasPrincipales/PantallaInicio.xaml.cs
+++ MediTrack.Frontend/Vistas/PantallasPrincipales/PantallaInicio.xaml.cs
@@ -10,8 +10,8 @@ public partial class PantallaInicio : ContentPage
         BindingContext = new PantallaInicioViewModel();
     }
 
-    private async void BotonEscanear_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("pantallaScan"); // Usa la ruta definida en AppShell
-    }
+    //private async void BotonEscanear_Clicked(object sender, EventArgs e)
+    //{
+    //    await Shell.Current.GoToAsync("pantallaScan"); // Usa la ruta definida en AppShell
+    //}
 }
