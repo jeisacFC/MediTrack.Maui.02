54e0a08d4457bc203d544b505d3d55b34994250c
Jeremy Fernández <jeremy.fernandez.castro@est.una.ac.cr>
2025-06-08 07:13:42 -0600

    Comenta el boton del scan en el inicio de sesion

--- MediTrack.Frontend/Vistas/PantallasInicio/PantallaBienvenida.xaml.cs
+++ MediTrack.Frontend/Vistas/PantallasInicio/PantallaBienvenida.xaml.cs
@@ -17,21 +17,21 @@ namespace MediTrack.Frontend.Vistas.PantallasInicio
         private async void IrARegistro(object sender, EventArgs e)
             => await Shell.Current.GoToAsync("//registro");
 
-        private async void IrAPantallaEscaneo(object sender, EventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("=== INICIO IrAPantallaEscaneo ===");
-            try
-            {
-                System.Diagnostics.Debug.WriteLine($"Página actual antes de guardar: {Shell.Current.CurrentState.Location}");
-                NavigationService.GuardarPaginaActual();
+        //private async void IrAPantallaEscaneo(object sender, EventArgs e)
+        //{
+        //    System.Diagnostics.Debug.WriteLine("=== INICIO IrAPantallaEscaneo ===");
+        //    try
+        //    {
+        //        System.Diagnostics.Debug.WriteLine($"Página actual antes de guardar: {Shell.Current.CurrentState.Location}");
+        //        NavigationService.GuardarPaginaActual();
 
-                System.Diagnostics.Debug.WriteLine("Navegando a escaneo...");
-                await NavigationService.GoToAsync("///pantallaScan");
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine($"Error navegando: {ex.Message}");
-            }
-        }
+        //        System.Diagnostics.Debug.WriteLine("Navegando a escaneo...");
+        //        await NavigationService.GoToAsync("///pantallaScan");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine($"Error navegando: {ex.Message}");
+        //    }
+        //}
     }
 }
